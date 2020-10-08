b3d5715d47c31364c9c0db570e882db0a73593a9
Dimpal Shetty <46956063+dimpalshetty@users.noreply.github.com>
2020-10-08 22:20:23 +0530

    Update style.php
    Few sizing issues resolved

--- css/style.php
+++ css/style.php
@@ -5,6 +5,12 @@
 		box-sizing: border-box;
 		font-family: 'Mulish', sans-serif;
 	}
+	html, body{
+		overflow-x: hidden;
+		width: 100%;
+    margin: 0px;
+    padding: 0px;
+	}
 
 	.nav_style {
 		background-color: #a1cad5 !important;
@@ -68,7 +74,7 @@
 	}
 
 
-	@media(min-width:720px) {
+	@media(max-width:720px) {
 		.about {
 			display: flex;
 			justify-content: space-between;
@@ -83,6 +89,10 @@
 			justify-content: flex-start;
 
 		}
+		.col-lg-5 {
+			margin-left: 0 px;
+
+		}
 
 		.image_2 {
 			margin: 2%;
@@ -94,5 +104,20 @@
 			flex-direction: column;
 			margin: 2%;
 		}
+		.navbar-toggler-icon {
+        background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255,255,255, 1)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
+}
+.img-fluid{
+	width: 85%;
+	flex-wrap: wrap;
+	justify-content: center;
+	margin-left: -30px;
+	
+}
+.navbar{
+	padding:15px 0 0 25px; 
+        margin: 0;
+}
+		
 	}
 </style>
