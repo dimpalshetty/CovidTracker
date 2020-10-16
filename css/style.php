0220fa75d84073f5751dc9f38110d35b7d4b61e6
Mehul <69337738+rootpipe@users.noreply.github.com>
2020-10-16 09:29:53 +0530

    Update style.php

--- css/style.php
+++ css/style.php
@@ -108,7 +108,7 @@
         background-image: url("data:image/svg+xml;charset=utf8,%3Csvg viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath stroke='rgba(255,255,255, 1)' stroke-width='2' stroke-linecap='round' stroke-miterlimit='10' d='M4 8h24M4 16h24M4 24h24'/%3E%3C/svg%3E");
 }
 .img-fluid{
-	width: 85%;
+	width: 80%;
 	flex-wrap: wrap;
 	justify-content: center;
 	margin-left: -30px;
